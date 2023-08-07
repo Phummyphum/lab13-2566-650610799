aed14cd56026634307443706db4c1201c6a626a9
Phummy_phum <phumrapee.phum@gmail.com>
2023-08-07 23:40:40 +0700

    complete

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Phumrapee Tapwong"
+            studentId="650610799"
           />
         </div>
       </body>
